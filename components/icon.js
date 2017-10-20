bec27df28ee70b2b8b5a8b2ac4e52dd1265c4dd8
imDangerous <imflower80@gmail.com>
2017-10-20 14:22:19 +0900

    Update icon.js

--- components/icon.js
+++ components/icon.js
@@ -1,5 +1,5 @@
-import React, { Component, PropTypes } from 'react';
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Entypo from 'react-native-vector-icons/Entypo';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
